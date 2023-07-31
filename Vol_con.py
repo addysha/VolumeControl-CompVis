3cf9753f3d7be6c91c0843f4fa1c30bbcba2b014
addysha <addysharmap@gmail.com>
2023-07-31 14:19:09 +1200

    initilizing hand struc

--- Vol_con.py
+++ Vol_con.py
@@ -4,4 +4,16 @@ import mediapipe as mp
 
 capture = cv2.VideoCapture(0)
 mp_hads = mp.solutions.hands
-hands= mp_hands.Hands(static_image_mode=False, max_num_hands=1,)
+hands= mp_hands.Hands(static_image_mode=False, max_num_hands=1,
+                      min_detection_confidence=0.5,min_tracking_confidence=0.5)
+
+mp_drawing = mp.solutions.drawing_utils
+
+while True:
+    ret,frame = cap.read()
+    if not ret:
+        break
+
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
+
+    results = hands.process(image_rgb)
